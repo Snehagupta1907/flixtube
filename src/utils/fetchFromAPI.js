2c03ec862c959e4904899e0153166850f3e2d05b
Snehagupta1907 <snehagupta98930@gmail.com>
2023-06-30 22:53:57 +0530

    fetched videos

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const BASE_URL='https://youtube-v31.p.rapidapi.com';
 
 const options = {
@@ -8,7 +9,7 @@ const options = {
       maxResults:'50',
     },
     headers: {
-      'X-RapidAPI-Key': process.env.RAPID_LEY,
+      'X-RapidAPI-Key': '291f086901mshc836e034fb8f9d0p14cc85jsnfa7cada8f093',
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
